f74c9333dd30aac3241be8e96019224e16db94f3
mjdenham <mjdenham@5ead7089-b63d-dc5a-5360-884d33c71362>
2011-09-20 09:42:05 +0000

    wrong level

--- tags/build-00.07.01/src/net/bible/android/control/event/apptobackground/AppToBackgroundEvent.java
+++
@@ -1,5 +0,0 @@
-package net.bible.android.control.event.apptobackground;
-
-public class AppToBackgroundEvent {
-
-}
--- tags/build-00.07.01/src/net/bible/android/control/event/apptobackground/AppToBackgroundListener.java
+++
@@ -1,7 +0,0 @@
-package net.bible.android.control.event.apptobackground;
-
-import java.util.EventListener;
-
-public interface AppToBackgroundListener extends EventListener {
-	void applicationNowInBackground(AppToBackgroundEvent e);
-}
